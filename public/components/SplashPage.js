0b1d6a931f43b2cf74b4d702dbff41e6cf0ed541
ccinkosky <corey.cinkosky@gmail.com>
2022-01-20 22:58:12 -0700

    Updated Splash Page
    Updated Splash Page

--- public/components/SplashPage.js
+++ public/components/SplashPage.js
@@ -1,71 +1,28 @@
-class SplashPage extends React.Component {
+function SplashPage (props) {
 
-    constructor (props) {
-        super(props);
-        this.state = { data : {} };
-        this.navigate = this.props.navigate;
-        this.mounted = false;
+    function navigate (loc) {
+        props.navigate(loc);
     }
 
-    /* 
-     * When the component is mounted, call the updateSplashPage function
-     * Although it's not really necessary for the splash page content to 
-     * stay updated after the component has mounted, this is an example
-     * of how you can use setInterval to fetch data from the API at specific
-     * intervals and update the state of the component
-     */
-    componentDidMount () {
-        this.updateSplashPage();
-        setInterval(function () {
-            this.updateSplashPage();
-        }.bind(this),10000);
-    }
-
-    /*
-     * This sends the json '{"fetch":"splashPage"}' in the body
-     * of a POST request to /api/index/splash_page. This will create an 
-     * instance of IndexController and call it's function SplashPageAction. 
-     * SplashPageAction returns the "splashPage" array from config.json.
-     * It then updates the state using the data returned from the api,
-     * which automatically updates the splash page
-     */
-    async updateSplashPage () {
-        try {
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ fetch : 'splashPage' })
-            };
-            const res = await fetch("/api/index/splash_page", requestOptions);
-            const data = await res.json();
-            this.setState({ data : data });
-            if (!this.mounted) {
-                $(".component-container").fadeIn(300,function () {
-                    this.mounted = true;
-                });
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
-    render () {
-        return (
-            <div className="component-container">
-                <div className="splash-container">
-                    <div className="splash">
-                        <h1 className="splash-head">{this.state.data.head}</h1>
-                        <p className="splash-subhead">
-                            {this.state.data.subhead}
-                        </p>
-                        <p>
-                            <Link className="pure-button pure-button-primary" onClick={()=>this.navigate(this.state.data.buttonLink)}>
-                                {this.state.data.buttonText} <i className="fas fa-sign-out-alt" aria-hidden="true"></i>
-                            </Link>
-                        </p>
+    return (
+        <div className="component-container">
+            <div className="splash-container">
+                <div className="splash">
+                    <h1 className="splash-head">CinkoMVC</h1>
+                    <div className="markdown-body splash-body" style={{textAlign: "left"}}>
+                        A light weight PHP MVC framework with React via CDN setup on the front end.
+                        Created with simplicity in mind, it's an easy tool to get associated with MVC
+                        frameworks and/or React while still being able to work with a good old LAMP stack.
+                        The front end is currently setup as a single page React app while the back end acts
+                        as an API that serves up JSON for the front end to consume.
                     </div>
+                    <p>
+                        <Link className="get-started pure-button pure-button-primary" onClick={()=>navigate("/README.md")}>
+                            Get Started <i className="fas fa-sign-out-alt" aria-hidden="true"></i>
+                        </Link>
+                    </p>
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
 }
